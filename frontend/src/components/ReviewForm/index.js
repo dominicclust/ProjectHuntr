facc91700b133cc97484898e97776e05243bb837
dominicclust <dclust34@gmail.com>
2022-06-06 00:23:51 -0500

    added import to review form. trying once more

--- frontend/src/components/ReviewForm/index.js
+++ frontend/src/components/ReviewForm/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-import { postReview } from '../../store/reviews'
+import { postReview, getReviews } from '../../store/reviews'
 import { useSelector, useDispatch } from 'react-redux'
 import './ReviewForm.css'
 
